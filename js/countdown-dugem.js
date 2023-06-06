d6056c52fb8311452475af9a9c37fbb636832ea7
AJ DeMarco <79189312+ajdemarco95@users.noreply.github.com>
2023-06-06 11:36:09 -0400

    removed bs error

--- js/countdown-dugem.js
+++ js/countdown-dugem.js
@@ -20,17 +20,17 @@
                  document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
 
              //do something later when date is reached
-             if (distance < 0) {
-                 let headline = document.getElementById("headline"),
-                     countdown = document.getElementByClassName("countdown"),
-                     content = document.getElementById("content");
+            //  if (distance < 0) {
+            //      let headline = document.getElementById("headline"),
+            //          countdown = document.getElementByClassName("countdown"),
+            //          content = document.getElementById("content");
 
-                 headline.innerText = "It's my birthday!";
-                 countdown.style.display = "none";
-                 content.style.display = "block";
+            //      headline.innerText = "It's my birthday!";
+            //      countdown.style.display = "none";
+            //      content.style.display = "block";
 
-                 clearInterval(x);
-             }
+            //      clearInterval(x);
+            //  }
              //seconds
          }, 0)
          }());
